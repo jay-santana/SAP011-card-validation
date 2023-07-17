e19971e9e5db50b7054a30e77577d1859a4c5515
jay santana <jayannyrayara@gmail.com>
2023-07-17 19:50:42 -0300

    Adicionada estrutura e estilo à página 3

--- src/validator.js
+++ src/validator.js
@@ -4,11 +4,11 @@ const validator = {
 
 export default validator;
 
-const buttonOneFirstPag = document.querySelector('.button-menu-container')
+const buttonOnePag1 = document.querySelector('.button-menu-container')
 
 
-buttonOneFirstPag.addEventListener('click', informStore)
+buttonOnePag1.addEventListener('click', informStore)
 
 function informStore() {
-  buttonOneFirstPag.classList.add('hide')
+  buttonOnePag1.classList.add('hide')
 }
