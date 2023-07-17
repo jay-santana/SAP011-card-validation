e79abdfaa20c1c5c52c91a92fcff3ae758a80313
jay santana <jayannyrayara@gmail.com>
2023-07-17 10:48:25 -0300

    Primeiro commit - Criei uma pasta com imagens e adicionei uma imagem de plano de fundo utilizando background-image em CSS; - Criei uma pasta de fonte e adicionei uma fonte ao projeto utilizando @font-face em CSS; - Criei uma div class="container..." para cada página do projeto, e adicionei os elementos que irão compor cada página do projeto; Página 1: - Criei uma div e adicionei todas informações da página dentro dela; - Adicionei um título (h1) e um subtítulo (h3) e coloquei eles dentro de uma span; - Adicionei os botões na página e coloquei eles dentro de uma div. Página 2: - Criei uma div e adicionei todas informações da página dentro dela; - Adicionei um título (h2) e coloquei eles dentro de uma span; - Adicionei um texto a página e coloquei eles dentro de uma div; - Adicionei um botão de "Voltar ao menu" e coloquei ele dentro de uma div. Página 3: - Adicionei um título (h2) e coloquei eles dentro de uma span; - Criei uma div e adicionei todas informações sobre o cartão dentro dela; - Adicionei um primeiro form, adicionei as informações referentes a "número do cartão" e "títular do cartão" e coloquei cada uma dentro de uma div;   - Adicionei um label for para "número do cartão" e um input type id com um placeholder "4154 2560 8541 9631".   - Adicionei um label for para "títular do cartão" e um input type id com um placeholder "Maria da Silva". - Adicionei um segundo form, adicionei as informações referentes a "Validade" e "CVV" e coloquei cada uma dentro de uma div;   - Adicionei um label for para "Validade" e um input type id com um placeholder "06/25".   - Adicionei um label for para "CVV" e um input type id com um placeholder "123". - Adicionei um botão de "Validar" e coloquei ele dentro de uma div. Página 4: - Criei uma div e adicionei todas informações da página dentro dela; - Adicionei dois botões "Voltar ao menu" e "Confirmar compra", e coloquei eles dentro de uma div.

--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,12 @@ const validator = {
 };
 
 export default validator;
+
+const buttonOneFirstPag = document.querySelector('.button-menu-container')
+
+
+buttonOneFirstPag.addEventListener('click', informStore)
+
+function informStore() {
+  buttonOneFirstPag.classList.add('hide')
+}
