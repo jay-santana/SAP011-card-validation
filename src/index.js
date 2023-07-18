d3a78e6cfef56c782aa455e2e773d1ace0d73c3e
jay santana <jayannyrayara@gmail.com>
2023-07-18 12:17:23 -0300

    Copia dos códigos do validation.js para index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const buttonOnePag1 = document.querySelector('.btn1')
+const informsPag1 = document.querySelector('.container-one')
+const informsPag2 = document.querySelector('.container-two')
+const buttonReturnMenuOne = document.querySelector('.return-menu')
+const buttonFivePag1 = document.querySelector('.btn5')
+const informsPag3 = document.querySelector('.container-three')
+const buttonValidation = document.querySelector('.validation')
+const informsPag4 = document.querySelector('.container-for')
+const buttonReturnMenuTwo = document.querySelector('.return-menu-two')
+
+
+// Funcionalidade dos botões das páginas
+buttonOnePag1.addEventListener('click', informStore)
+buttonReturnMenuOne.addEventListener('click', returnMenuOne)
+buttonFivePag1.addEventListener('click', idCard)
+buttonValidation.addEventListener('click', msgValidation)
+buttonReturnMenuTwo.addEventListener('click', returnMenuTwo)
+
+
+function informStore() {
+  informsPag1.classList.add('hide')
+  informsPag2.classList.remove('hide')
+}
+
+function returnMenuOne() {
+  informsPag1.classList.remove('hide')
+  informsPag2.classList.add('hide')
+}
+
+function idCard() {
+  informsPag1.classList.add('hide')
+  informsPag3.classList.remove('hide')
+}
+
+function msgValidation() {
+  informsPag3.classList.add('hide')
+  informsPag4.classList.remove('hide')
+}
+
+function returnMenuTwo() {
+  informsPag4.classList.add('hide')
+  informsPag1.classList.remove('hide')
+}
--- src/validator.js
+++ src/validator.js
@@ -1,48 +1,65 @@
-// const validator = {
-//   // ...
-// };
+const validator = {
+  // ...
+};
 
-// export default validator;
+export default validator;
 
-const buttonOnePag1 = document.querySelector('.btn1')
-const informsPag1 = document.querySelector('.container-one')
-const informsPag2 = document.querySelector('.container-two')
-const buttonReturnMenuOne = document.querySelector('.return-menu')
-const buttonForPag1 = document.querySelector('.btn5')
-const informsPag3 = document.querySelector('.container-three')
-const buttonValidation = document.querySelector('.validation')
-const informsPag4 = document.querySelector('.container-for')
-const buttonReturnMenuTwo = document.querySelector('.return-menu-two')
-
-
-buttonOnePag1.addEventListener('click', informStore)
-buttonReturnMenuOne.addEventListener('click', returnMenuOne)
-buttonForPag1.addEventListener('click', idCard)
-buttonValidation.addEventListener('click', msgValidation)
-buttonReturnMenuTwo.addEventListener('click', returnMenuTwo)
+// const buttonOnePag1 = document.querySelector('.btn1')
+// const informsPag1 = document.querySelector('.container-one')
+// const informsPag2 = document.querySelector('.container-two')
+// const buttonReturnMenuOne = document.querySelector('.return-menu')
+// const buttonFivePag1 = document.querySelector('.btn5')
+// const informsPag3 = document.querySelector('.container-three')
+// const buttonValidation = document.querySelector('.validation')
+// const informsPag4 = document.querySelector('.container-for')
+// const buttonReturnMenuTwo = document.querySelector('.return-menu-two')
 
 
-function informStore() {
-  informsPag1.classList.add('hide')
-  informsPag2.classList.remove('hide')
-}
+// // Funcionalidade dos botões das páginas
+// buttonOnePag1.addEventListener('click', informStore)
+// buttonReturnMenuOne.addEventListener('click', returnMenuOne)
+// buttonFivePag1.addEventListener('click', idCard)
+// buttonValidation.addEventListener('click', msgValidation)
+// buttonReturnMenuTwo.addEventListener('click', returnMenuTwo)
 
-function returnMenuOne() {
-  informsPag1.classList.remove('hide')
-  informsPag2.classList.add('hide')
-}
 
-function idCard() {
-  informsPag1.classList.add('hide')
-  informsPag3.classList.remove('hide')
-}
+// function informStore() {
+//   informsPag1.classList.add('hide')
+//   informsPag2.classList.remove('hide')
+// }
 
-function msgValidation() {
-  informsPag3.classList.add('hide')
-  informsPag4.classList.remove('hide')
-}
+// function returnMenuOne() {
+//   informsPag1.classList.remove('hide')
+//   informsPag2.classList.add('hide')
+// }
 
-function returnMenuTwo() {
-  informsPag4.classList.add('hide')
-  informsPag1.classList.remove('hide')
-}
+// function idCard() {
+//   informsPag1.classList.add('hide')
+//   informsPag3.classList.remove('hide')
+// }
+
+// function msgValidation() {
+//   informsPag3.classList.add('hide')
+//   informsPag4.classList.remove('hide')
+// }
+
+// function returnMenuTwo() {
+//   informsPag4.classList.add('hide')
+//   informsPag1.classList.remove('hide')
+// }
+
+//Alerta sobre não poder inserir um campos vazios as informações do usuário 
+
+
+//Ocultar todos os dígitos do número do cartão, excetuando os últimos 4 caractere
+
+//Algoritmo de Luhn
+//Inserir o número que desejamos validar, esse número deve conter 16 dígitos (contendo apenas caracteres numéricos, dígitos de [0-9]); 
+
+//Multiplicar por dois, todos os números que ocupam uma posição par;
+
+//Adicionar os dígitos do resultado para os números maiores ou iguais a 10 (16 => 1 + 6 = 7);
+
+//Verificar a validação do número, o número a verificar será válido se a soma de seus dígitos finais for um múltiplo de 10.
+
+//Exibir mensagem sobre a validação ou invalidação do cartão
